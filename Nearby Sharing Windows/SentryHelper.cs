4720553a96fa28844ca4a092963aa96202251ec5
Lukas Kurz <lukas@shortdev.de>
2023-05-18 16:19:03 +0200

    Workaround: Disable `r8` #94

--- Nearby Sharing Windows/SentryHelper.cs
+++ Nearby Sharing Windows/SentryHelper.cs
@@ -4,17 +4,9 @@ namespace Nearby_Sharing_Windows;
 
 internal static class SentryHelper
 {
-    static SentryHelper()
-    {
-#if DEBUG
-        _isInitialized = true;
-#endif
-    }
-
-    private static bool _isInitialized = false;
     public static void EnsureInitialized()
     {
-        if (_isInitialized)
+        if (SentrySdk.IsEnabled)
             return;
 
         SentrySdk.Init(options =>
@@ -22,7 +14,5 @@ internal static class SentryHelper
             options.Dsn = "https://47f9f6c3642149a5af942e8484e64fe1@o646413.ingest.sentry.io/6437134";
             options.TracesSampleRate = 0.7;
         });
-
-        _isInitialized = true;
     }
 }
