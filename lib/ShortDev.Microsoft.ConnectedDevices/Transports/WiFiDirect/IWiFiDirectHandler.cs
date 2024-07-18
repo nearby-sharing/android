53f4b37a772ae297d86693d0079f669b7a8fa424
Lukas Kurz <lukas@shortdev.de>
2024-07-18 10:34:12 +0200

    WiFiDirect handler

---
+++ lib/ShortDev.Microsoft.ConnectedDevices/Transports/WiFiDirect/IWiFiDirectHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+using System.Net.NetworkInformation;
+
+namespace ShortDev.Microsoft.ConnectedDevices.Transports.WiFiDirect;
+public interface IWiFiDirectHandler : IDisposable
+{
+    Task<IPAddress> ConnectAsync(EndpointInfo device, CancellationToken cancellationToken = default);
+
+    PhysicalAddress MacAddress { get; }
+    bool IsEnabled { get; }
+}
