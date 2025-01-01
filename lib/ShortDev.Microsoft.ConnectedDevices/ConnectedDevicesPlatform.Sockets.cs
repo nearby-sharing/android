3390897b0ae86e679181a5f152ed4796d69887a4
Lukas Kurz <lukas@shortdev.de>
2025-01-01 18:12:57 +0100

    Restructuring cdp class

---
+++ lib/ShortDev.Microsoft.ConnectedDevices/ConnectedDevicesPlatform.Sockets.cs
@@ -0,0 +1,44 @@
+﻿using ShortDev.Microsoft.ConnectedDevices.Transports;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ShortDev.Microsoft.ConnectedDevices;
+
+partial class ConnectedDevicesPlatform
+{
+    readonly ConcurrentDictionary<EndpointInfo, CdpSocket> _knownSockets = new();
+
+    void RegisterKnownSocket(CdpSocket socket)
+    {
+        Debug.Assert(!socket.IsClosed);
+
+        socket.Disposed += OnSocketClosed;
+        void OnSocketClosed()
+        {
+            socket.Disposed -= OnSocketClosed;
+
+            var couldRemove = _knownSockets.TryRemove(KeyValuePair.Create(socket.Endpoint, socket));
+            Debug.Assert(couldRemove);
+        }
+
+        _knownSockets.AddOrUpdate(
+            socket.Endpoint,
+            static (key, newSocket) => newSocket,
+            static (key, newSocket, currentSocket) => newSocket,
+            socket
+        );
+    }
+
+    bool TryGetKnownSocket(EndpointInfo endpoint, [MaybeNullWhen(false)] out CdpSocket socket)
+    {
+        if (!_knownSockets.TryGetValue(endpoint, out socket))
+            return false;
+
+        // ToDo: Alive check!!
+        if (socket.IsClosed)
+            return false;
+
+        return true;
+    }
+}
---
+++ lib/ShortDev.Microsoft.ConnectedDevices/ConnectedDevicesPlatform.Utils.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using ShortDev.Microsoft.ConnectedDevices.Encryption;
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ShortDev.Microsoft.ConnectedDevices;
+
+partial class ConnectedDevicesPlatform
+{
+    public static X509Certificate2 CreateDeviceCertificate([NotNull] CdpEncryptionParams encryptionParams)
+    {
+        using var key = ECDsa.Create(encryptionParams.Curve);
+        CertificateRequest certRequest = new("CN=Ms-Cdp", key, HashAlgorithmName.SHA256);
+        return certRequest.CreateSelfSigned(DateTimeOffset.Now, DateTimeOffset.Now.AddYears(5));
+    }
+
+    public static ILoggerFactory CreateLoggerFactory(string filePattern, LogLevel logLevel = LogLevel.Debug)
+        => LoggerFactory.Create(builder =>
+        {
+            builder.ClearProviders();
+
+            builder.SetMinimumLevel(logLevel);
+
+            builder.AddFile(filePattern, logLevel);
+        });
+}
