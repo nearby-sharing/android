a3bbcd9a631c1cc4140ac1f41a87509053139908
Lukas Kurz <lukas@shortdev.de>
2025-04-06 22:26:49 +0200

    Centralize send logic

---
+++ lib/ShortDev.Microsoft.ConnectedDevices/Messages/EmptyMessage.cs
@@ -0,0 +1,12 @@
+﻿namespace ShortDev.Microsoft.ConnectedDevices.Messages;
+
+internal readonly record struct EmptyMessage : IBinaryParsable<EmptyMessage>, IBinaryWritable
+{
+    public readonly void Write<TWriter>(ref TWriter writer) where TWriter : struct, IEndianWriter, allows ref struct
+    {
+        // No content to write
+    }
+
+    public static EmptyMessage Parse<TReader>(ref TReader reader) where TReader : struct, IEndianReader, allows ref struct
+        => default;
+}
