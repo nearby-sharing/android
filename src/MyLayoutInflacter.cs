3650670d8f4c9d9171245c8de24431acf76ef93b
Lukas Kurz <lukas@shortdev.de>
2025-09-17 22:08:48 +0200

    Bump dependencies

---
+++ src/MyLayoutInflacter.cs
@@ -0,0 +1,26 @@
+﻿using Android.Content;
+using Android.Util;
+using Android.Views;
+
+namespace NearShare;
+
+internal sealed class MyLayoutInflacter(LayoutInflater original, Context? newContext) : LayoutInflater(original, newContext)
+{
+    public override LayoutInflater? CloneInContext(Context? newContext)
+        => new MyLayoutInflacter(this, newContext);
+
+    public override View? OnCreateView(Context viewContext, View? parent, string name, IAttributeSet? attrs)
+    {
+        return base.OnCreateView(viewContext, parent, name, attrs);
+    }
+
+    protected override View? OnCreateView(string? name, IAttributeSet? attrs)
+    {
+        return base.OnCreateView(name, attrs);
+    }
+
+    protected override View? OnCreateView(View? parent, string? name, IAttributeSet? attrs)
+    {
+        return base.OnCreateView(parent, name, attrs);
+    }
+}
