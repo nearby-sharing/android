f4e644327315cd8730dbdda2b98f7e0168b55bad
Lukas Kurz <lukas@shortdev.de>
2026-01-15 21:35:35 +0100

    Add missing values to `ControlMessageType`

--- lib/ShortDev.Microsoft.ConnectedDevices/Messages/Control/ControlMessageType.cs
+++ lib/ShortDev.Microsoft.ConnectedDevices/Messages/Control/ControlMessageType.cs
@@ -4,7 +4,11 @@ public enum ControlMessageType : byte
 {
     StartChannelRequest = 0,
     StartChannelResponse = 1,
+    StopChannelRequest = 2,
+    EnumerateAppsRequest = 3,
     EnumerateAppsReponse = 4,
     EnumerateAppTargetNamesRequest = 5,
-    EnumerateAppTargetNamesResponse = 6
+    EnumerateAppTargetNamesResponse = 6,
+    ChannelAuthorizationRequest = 7,
+    ChannelAuthorizationResponse = 8,
 }
