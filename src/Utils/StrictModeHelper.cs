685c2fe92992a685b9274fe4cbc53a36d4e127be
Lukas Kurz <lukas@shortdev.de>
2026-04-22 11:50:18 +0200

    feat: enable strict mode in debug config (#247)
    Enable [Strict
    Mode](https://developer.android.com/reference/android/os/StrictMode) in
    the debug configuration to help debug ANR.

---
+++ src/Utils/StrictModeHelper.cs
@@ -0,0 +1,28 @@
+﻿#if DEBUG
+
+using Android.OS;
+using static Android.OS.StrictMode;
+
+namespace NearShare.Utils;
+
+internal static class StrictModeHelper
+{
+    public static void EnableStrictMode()
+    {
+        SetThreadPolicy(
+            new ThreadPolicy.Builder()
+                .DetectAll()
+                .PenaltyLog()
+                .PenaltyFlashScreen()
+                .Build()
+        );
+        SetVmPolicy(
+            new VmPolicy.Builder()
+                .DetectAll()
+                .PenaltyLog()
+                .Build()
+        );
+    }
+}
+
+#endif
