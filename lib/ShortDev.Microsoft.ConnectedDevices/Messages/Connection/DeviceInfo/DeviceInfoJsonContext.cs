52e0fa7da4d297347eb5714e79e8d1e06589a47d
Lukas Kurz <lukas@shortdev.de>
2026-05-14 17:11:08 +0200

    chore: trimming (#248)
    Enable trimming to reduce app size.
    - json source generator
    - pregenerated bond serialization logic

---
+++ lib/ShortDev.Microsoft.ConnectedDevices/Messages/Connection/DeviceInfo/DeviceInfoJsonContext.cs
@@ -0,0 +1,6 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ShortDev.Microsoft.ConnectedDevices.Messages.Connection.DeviceInfo;
+
+[JsonSerializable(typeof(CdpDeviceInfo))]
+internal sealed partial class DeviceInfoJsonContext : JsonSerializerContext;
---
+++ lib/ShortDev.Microsoft.ConnectedDevices/Serialization/ThrowExpression.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Bond.Expressions;
+
+/// <summary>
+/// ThrowExpression is a utility that makes it easy to create expressions that throw exceptions.
+/// </summary>
+internal static class ThrowExpression
+{
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static void ThrowInvalidTypeException(BondDataType actualType)
+    {
+        throw new InvalidDataException(string.Format("Invalid type {0}", actualType));
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static void ThrowInvalidTypeException(BondDataType expectedType, BondDataType actualType)
+    {
+        throw new InvalidDataException(string.Format("Invalid type {0}, expected {1}", actualType, expectedType));
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static void ThrowInvalidDataException(string message)
+    {
+        throw new InvalidDataException(message);
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static void ThrowRequiredFieldMissingException(string schema, string field)
+    {
+        throw new InvalidDataException(string.Format("Required field {0}.{1} missing", schema, field));
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static void ThrowRequiredFieldsMissingException(List<string> names, int index)
+    {
+        throw new InvalidDataException(string.Format("Required field {0} missing", names[index]));
+    }
+}
